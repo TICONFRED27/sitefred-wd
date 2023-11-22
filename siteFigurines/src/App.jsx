02e9062eda09f6ea8196aee3f8aae78b56163f96
FREDERIC TICON <fred.ticon@gmail.com>
2023-11-22 12:02:13 +0100

    Update App.jsx

--- siteFigurines/src/App.jsx
+++ siteFigurines/src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import {} from "react";
+import "./App.scss";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>fred essai</div>
