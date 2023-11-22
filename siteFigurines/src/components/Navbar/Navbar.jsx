e66c088c967d30e5c42721899f541d9490667af5
FREDERIC TICON <fred.ticon@gmail.com>
2023-11-22 17:17:18 +0100

    Update Navbar.jsx

--- siteFigurines/src/components/Navbar/Navbar.jsx
+++ siteFigurines/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,26 @@
 import "./Navbar.scss";
 
-function Navbar() {}
+function Navbar() {
+  return (
+    <>
+      <nav className="navbar">
+        <ul className="navbar__ul">
+          <li className="navbar__li">
+            <a href="#">Acceuil</a>
+          </li>
+          <li className="navbar__li">
+            <a href="#">Ateliers</a>
+          </li>
+          <li className="navbar__li">
+            <a href="#">Catalogue</a>
+          </li>
+          <li className="navbar__li">
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
 
 export default Navbar;
