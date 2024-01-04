c90f9e8b007242be905686c793b1adbd95c54fed
FREDERIC TICON <fred.ticon@gmail.com>
2024-01-04 16:31:50 +0100

    Update App.jsx

--- siteFigurines/src/App.jsx
+++ siteFigurines/src/App.jsx
@@ -1,11 +1,15 @@
 import {} from "react";
 import Navbar from "./components/Navbar/Navbar";
 import "./App.scss";
+import Acceuil from "./pages/Acceuil/Acceuil";
+import Ateliers from "./pages/Ateliers/Ateliers";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Acceuil />
+      <Ateliers />
     </>
   );
 }
