2afd59dde81ca4782a155f92e1f8afc253f97bc7
FREDERIC TICON <fred.ticon@gmail.com>
2024-01-31 10:20:14 +0100

    Update Navbar.jsx

--- siteFigurines/src/components/Navbar/Navbar.jsx
+++ siteFigurines/src/components/Navbar/Navbar.jsx
@@ -6,16 +6,24 @@ function Navbar() {
       <nav className="navbar">
         <ul className="navbar__ul">
           <li className="navbar__li">
-            <a href="#">Acceuil</a>
+            <a className="navbar__a" href="#">
+              Acceuil
+            </a>
           </li>
           <li className="navbar__li">
-            <a href="#">Ateliers</a>
+            <a className="navbar__a" href="#">
+              Ateliers
+            </a>
           </li>
           <li className="navbar__li">
-            <a href="#">Catalogue</a>
+            <a className="navbar__a" href="#">
+              Catalogue
+            </a>
           </li>
           <li className="navbar__li">
-            <a href="#">Contact</a>
+            <a className="navbar__a" href="#">
+              Contact
+            </a>
           </li>
         </ul>
       </nav>
